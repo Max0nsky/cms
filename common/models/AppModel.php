296d63c55589221c5761658563d6b708744a68b1
Max0nsky <maksimum.99@mail.ru>
2022-08-17 23:06:32 +0300

    modify image upload

--- common/models/AppModel.php
+++ common/models/AppModel.php
@@ -5,20 +5,30 @@ namespace common\models;
 use \yii\db\ActiveRecord;
 use yii\helpers\Inflector;
 use yii\imagine\Image;
+use yii\web\UploadedFile;
 
 abstract class AppModel extends ActiveRecord
 {
-    public function uploadImage($imagesContainer)
+    const IMG_WIDTH = 1200;
+    const IMG_HEIGHT = 900;
+    const IMG_NAME_DEFAULT = 'preview';
+
+    public function uploadImage(UploadedFile $imagesContainer, string $imageName = self::IMG_NAME_DEFAULT, int $width = self::IMG_WIDTH, int $height = self::IMG_HEIGHT)
     {
         if (!empty($imagesContainer)) {
-            $this->removeImage($this->getImage());
+
+            $imageName = !empty($imageName) ? $imageName : self::IMG_NAME_DEFAULT;
+            $width  = ($width > 0) ? $width : self::IMG_WIDTH;
+            $height = !($height > 0) ? $height : self::IMG_HEIGHT;
+
+            $this->removeImage($this->getImageByName($imageName));
             $path = $_SERVER['DOCUMENT_ROOT'] . '/files/images/store/' . $imagesContainer->baseName . '.' . $imagesContainer->extension;
-            Image::resize($imagesContainer->tempName, 1200, 900, true, false)->save($path);
-            $this->attachImage($path, true, $this->tableName() . '_image');
+            Image::resize($imagesContainer->tempName, $width, $height, true, false)->save($path);
+            $this->attachImage($path, true, $imageName);
             @unlink($path);
         }
     }
-    
+
     public static function generateSlug($name)
     {
         return Inflector::slug($name);
