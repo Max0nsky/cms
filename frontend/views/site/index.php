d3b3406b2877226a359ebc31d40b25e301c6fb51
Max0nsky <maksimum.99@mail.ru>
2023-02-06 21:37:09 +0300

    test

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Yii2 приложение</h1>
 
-        <p class="lead">REST-api</p>
+        <p class="lead">REST-api <a target="_blank" href="https://www.yiiframework.com/doc/guide/2.0/ru/rest-quick-start">документация</a></p>
 
         <p><a class="btn btn-lg btn-success" target="_blank" href="http://www.yiiframework.com/doc/">Документация Yii</a></p>
     </div>
